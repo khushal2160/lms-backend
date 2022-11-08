000f147b607a75d108a04419dd4539ba91591292
khushal2160 <khushal2160@gmail.com>
2022-11-08 23:19:14 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 import appConfig from './config/app.config'
 import { APIErrorHandler } from './config/error.config'
 import { routes } from './routes/index.route'
 
 const app = express()
 
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: '*'
+};
+
+app.use(cors(options))
 app.use(express.json())
 app.use('/api', routes)
 app.use(APIErrorHandler)
